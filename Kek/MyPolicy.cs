263b8b670038af2550dc0740e8786415cb196822
SDK <fusiontreetwitch@gmail.com>
2016-07-24 10:28:58 -0400

    First Commit

---
+++ Kek/MyPolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Net;
+
+public class MyPolicy : ICertificatePolicy
+{
+    public bool CheckValidationResult(ServicePoint srvPoint,
+      X509Certificate certificate, WebRequest request,
+      int certificateProblem)
+    {
+        return true;
+    }
+}
